188aa0d5444d662c2da613634ba964f4785131ee
Mermersk <ic4ruz39@gmail.com>
2018-06-18 20:26:52 +0200

    First upload of reworked Duckentry

---
+++ conf.lua
@@ -0,0 +1,21 @@
+function love.conf(t)
+
+	kerfi = love._os  --Kikir hvaða stýrikerfi er áður en allt löve vélin og kóðin keyrist
+    --kerfi = "Android"
+
+	if kerfi == "Android" then
+      t.window.width = 0
+      t.window.height = 0
+	    t.title = "Duckentry"
+      t.author = "Mermersk"
+	end
+
+	if kerfi == "Windows" or "OS X" or "Linux" then
+	  t.window.width = 480
+		t.window.height = 320
+		t.title = "Duckentry"
+    t.author = "Mermersk"
+
+	end
+
+end
---
+++ main.lua
@@ -0,0 +1,16 @@
+--Duckentry 2.0 rewrite to make a clearer, more efficient and cleaner version of Duckentry. OOP based design.
+
+function love.load()
+
+
+end
+
+function love.update(dt)
+
+
+end
+
+function love.draw()
+
+
+end
